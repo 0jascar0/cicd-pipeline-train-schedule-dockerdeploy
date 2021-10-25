011aa7be0504abbb4699e3fd02518e296afa50e2
fabriziav <75419607+fabriziav@users.noreply.github.com>
2021-10-25 17:26:27 +0200

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,13 @@
+variable "admin_username" {
+  type        = string
+  description = "username for linux machine"
+  default     = "admin"
+}
+variable "admin_password" {
+  type        = string
+  description = "username for linux machine"
+  default     = "Cpwins1!"
+}
+
+
+
